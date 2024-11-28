190431c46b82f85bca3a866b9c6fef7e68f20194
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-28 23:26:02 +1200

    controller class added

--- src/main/java/com/example/proyect1/orders/Orders.java
+++ src/main/java/com/example/proyect1/orders/Orders.java
@@ -1,14 +1,28 @@
 package com.example.proyect1.orders;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
 public class Orders {
 }
 
+@Controller
+@ResponseBody
+class OrdersController {
+    private final OrderRepository repository;
+
+    OrdersController(OrderRepository repo) {
+        this.repository = repo;
+    }
+}
+
 interface OrderRepository extends ListCrudRepository<Order, Integer> {}
 
+@Table("orders_line_items")
 record LineItem (@Id Integer id, int product, int quantity) {}
 record Order(@Id Integer id, Set<LineItem> lineItems) {}
