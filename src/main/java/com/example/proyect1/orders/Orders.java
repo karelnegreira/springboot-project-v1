c67bceeceb9c97ffd202890953f08dc9618d74d7
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-28 23:05:03 +1200

    repository and records added into Order class

--- src/main/java/com/example/proyect1/orders/Orders.java
+++ src/main/java/com/example/proyect1/orders/Orders.java
@@ -1,9 +1,14 @@
 package com.example.proyect1.orders;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+
 import java.util.Set;
 
 public class Orders {
 }
 
-record LineItem (Integer id, int product, int quantity) {}
-record Order(Integer id, Set<LineItem> lineItems) {}
+interface OrderRepository extends ListCrudRepository<Order, Integer> {}
+
+record LineItem (@Id Integer id, int product, int quantity) {}
+record Order(@Id Integer id, Set<LineItem> lineItems) {}
