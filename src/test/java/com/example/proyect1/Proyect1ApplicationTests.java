f8c418469cc9b5f4fb21dec66206b62fd92b83f9
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-28 23:10:05 +1200

    repository and records added into Order class

---
+++ src/test/java/com/example/proyect1/Proyect1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyect1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
